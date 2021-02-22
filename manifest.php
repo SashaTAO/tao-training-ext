1fa4863af4bd92ee8c5993171df1b3b43ff320b6
Oleksandr Khitko <oleksandr@taotesting.com>
2021-02-22 17:48:47 +0100

    Create a new extension.

---
+++ manifest.php
@@ -0,0 +1,42 @@
+<?php
+/**  
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * 
+ * Copyright (c) 2021 (original work) Open Assessment Technologies SA;
+ *               
+ * 
+ */
+
+/**
+ * Generated using taoDevTools 6.10.0
+ */
+return [
+    'name' => 'taoTrainingExt',
+    'label' => 'Training extension',
+    'description' => 'Training extension',
+    'license' => 'GPL-2.0',
+    'version' => '0.1.0',
+    'author' => 'Open Assessment Technologies SA',
+    'requires' => [
+        'generis' => '>=13.14.1',
+        'tao' => '>=46.14.4'
+    ],
+    'acl' => [],
+    'install' => [],
+    'uninstall' => [],
+    'routes' => [],
+    'constants' => [],
+    'extra' => []
+];
